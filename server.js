8dd291f5475745961675aa118e06debe13dde301
Sam <transferdata76@gmail.com>
2023-03-13 11:23:57 -0700

    seeded sample songs into the database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Songs = require('./models/tabs');
 require('dotenv').config();
 
 // connect to mongo
@@ -30,6 +31,34 @@ app.get('/edit', (req, res) => {
 });
 
 // S
+
+// Database seeding
+const sampleSongs = [
+    {
+      title: 'Big Bottom',
+      artist: 'Spinal Tap',
+      img: 'https://images.genius.com/9b6f3ca4dc0ff6c3b35fb22bdeed41a3.442x500x1.jpg',
+      notation: 1234,
+      comment: 'A cool one'
+    },
+    {
+      title: 'Back to Basom',
+      artist: 'Ween',
+      img: 'https://i.discogs.com/U-nFSOQAw0c89WCDLHRMz4Ri-9iN1hU9qgK3GkUXfKE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3MDIy/OC0xNDA4NzEzMzQ2/LTg0ODEuanBlZw.jpeg',
+      notation: 5678,
+      comment: 'Acoustic guitar favorite'
+    }
+  ];
+
+// Using the Songs schema to add this information to the database
+Songs.insertMany(sampleSongs, (err, inserted) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Sample songs seeded successfully')
+    }
+});
+
 
 
 // creating port
