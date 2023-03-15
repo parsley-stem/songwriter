d993ca7850e62b2b9b1a40cfb7adefd243b1ac93
Sam <transferdata76@gmail.com>
2023-03-15 16:18:57 -0700

    improved editor functionality, formatted code with prettier

--- models/tabs.js
+++ models/tabs.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const tabSchema = new mongoose.Schema({
-    artist: String,
-    title: {
-        type: String,
-        required: true
-    },
-    author: String,
-    img: String,
-    notation: {
-        type: String,
-        required: true
-    },
-    comment: String,
+  artist: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  img: String,
+  notation: {
+    type: String,
+    required: true,
+  },
+  comment: String,
 });
 
-const Songs = mongoose.model('Songs', tabSchema)
+const Songs = mongoose.model("Songs", tabSchema);
 module.exports = Songs;
--- server.js
+++ server.js
@@ -1,82 +1,92 @@
 // require dependencies
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const Songs = require('./models/tabs');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Songs = require("./models/tabs");
+require("dotenv").config();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
 // connect to mongo
-const mongoURI = 'mongodb+srv://ykhieml:oSjTfgkCvJSTULC1@cluster0.xp68xnd.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI =
+  "mongodb+srv://ykhieml:oSjTfgkCvJSTULC1@cluster0.xp68xnd.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, () => {
-    console.log('Connected to mongoD')
+  console.log("Connected to mongoD");
 });
 
 // error and success
 const db = mongoose.connection;
-db.on('error', (err) => console.log(`${err.message} MongoDB is not running`));
-db.on('connected', () => console.log('Mongo connected'));
-db.on('disconnected', () => console.log('Mongo disconnected'));
+db.on("error", (err) => console.log(`${err.message} MongoDB is not running`));
+db.on("connected", () => console.log("Mongo connected"));
+db.on("disconnected", () => console.log("Mongo disconnected"));
 
 // I
-app.get('/', (req, res) => {
-    Songs.find({}, (err, allSongs) => {
-        res.render('index.ejs', {
-            songs: allSongs
-        })
-    })
+app.get("/", (req, res) => {
+  Songs.find({}, (err, allSongs) => {
+    res.render("index.ejs", {
+      songs: allSongs,
+    });
+  });
 });
 
 // N
-app.get('/new', (req, res) => {
-    res.render('new.ejs')
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
 });
 
 // D
 // U
-app.put('/:id', (req,res) => {
-    Songs.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true},
-        (err, updatedSong) => {
-            res.redirect(`/${req.params.id}`)
-        }
-    )
-})
+app.put("/:id", (req, res) => {
+  Songs.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedSong) => {
+      res.redirect(`/${req.params.id}`);
+    }
+  );
+});
 
 // C
+app.post("/", (req, res) => {
+  Songs.create(req.body, (err, createdSong) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
 // E
-app.get('/:id/edit', (req, res) => {
-    Songs.findById(req.params.id, (err, foundSong) => {
-        if (err) {
-            console.log(err)
-            res.redirect(`/${foundSong._id}`)
-        } else {
-            res.render('edit.ejs', {
-                song: foundSong
-            })
-        }
-    })
+app.get("/:id/edit", (req, res) => {
+  Songs.findById(req.params.id, (err, foundSong) => {
+    if (err) {
+      console.log(err);
+      res.redirect(`/${foundSong._id}`);
+    } else {
+      res.render("edit.ejs", {
+        song: foundSong,
+      });
+    }
+  });
 });
 
 // S
-app.get('/:id', (req, res) => {
-    Songs.findById(req.params.id, (err, foundSong) => {
-        if (err) {
-            console.log(err)
-            res.redirect('/')
-        } else {
-            res.render('show.ejs',
-            { song: foundSong })
-        }
-    })
+app.get("/:id", (req, res) => {
+  Songs.findById(req.params.id, (err, foundSong) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/");
+    } else {
+      res.render("show.ejs", { song: foundSong });
+    }
+  });
 });
 
 // Database seeding
@@ -111,5 +121,5 @@ app.get('/:id', (req, res) => {
 // // creating port
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
